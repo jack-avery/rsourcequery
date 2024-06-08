7466a94f4cd420264205dd0130d9d35080605d15
jack-avery <email.jack.avery@gmail.com>
2024-06-08 16:57:59 -0400

    add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+//! Pure Rust async implementation of the [Source A2S_INFO Query Protocol](https://developer.valvesoftware.com/wiki/Server_queries#A2S_INFO)
+pub mod error;
+pub mod packet;
+pub mod query;
