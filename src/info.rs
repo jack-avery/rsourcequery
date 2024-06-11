2fa37d5df200f47f204fc489e77f49dca39b057e
jack-avery <email.jack.avery@gmail.com>
2024-06-10 20:15:39 -0400

    move parsing to packet.rs; separate query

--- src/info.rs
+++ src/info.rs
@@ -8,9 +8,20 @@ use tokio::time::timeout;
 
 use crate::error::SourceQueryError;
 
-use crate::packet::{RequestPacket, ResponsePacket, PacketType};
+use crate::packet::{
+    RequestPacket,
+    ResponsePacket,
+    PacketType,
+    get_string,
+    get_u8,
+    get_u16,
+    get_u64
+};
 
-/// Server information as obtained by [query].
+/// Default timeout duration if using [`query`] as opposed to [`query_timeout_duration`]
+const DEFAULT_TIMEOUT_SECS: u64 = 5;
+
+/// [`ServerInfo`] - server information as obtained by [`query`] or [`query_timeout_duration`]
 #[derive(Debug)]
 pub struct ServerInfo {
     /// A2S_INFO protocol version
@@ -64,60 +75,7 @@ pub struct ServerInfo {
 }
 
 impl ServerInfo {
-    /// Get the value of a null-terminated string
-    /// with index 0 at `offset` in an array of bytes.
-    /// 
-    /// Mutates `offset` to the index after the null-termination byte.
-    fn get_string(data: &[u8], offset: &mut usize) -> Result<String, SourceQueryError> {
-        let start_offset: usize = *offset;
-        let mut end_offset: usize = *offset;
-
-        while let Some(c) = data.get(end_offset) {
-            end_offset += 1;
-            if c == &0u8 {
-                break;
-            }
-        }
-        *offset = end_offset;
-
-        Ok(std::str::from_utf8(&data[start_offset..end_offset-1])?.to_string())
-    }
-
-    /// Get the [u8] at index `offset` from `data`.
-    /// 
-    /// Mutates `offset` to the index after the byte.
-    fn get_u8(data: &[u8], offset: &mut usize) -> u8 {
-        let byte: u8 = data[*offset];
-        *offset += 1;
-        byte
-    }
-
-    /// Get 2 bytes (as a [u16]) at index `offset` from `data`.
-    /// 
-    /// Mutates `offset` to the index after the bytes.
-    fn get_u16(data: &[u8], offset: &mut usize) -> u16 {
-        let bytes: &[u8] = &data[*offset..=*offset + 1];
-        *offset += 2;
-        ((bytes[1] as u16) << 8) | (bytes[0] as u16)
-    }
-
-    /// Get 8 bytes (as a [u64]) at index `offset` from `data`.
-    /// 
-    /// Mutates `offset` to the index after the bytes.
-    fn get_u64(data: &[u8], offset: &mut usize) -> u64 {
-        let bytes: &[u8] = &data[*offset..*offset + 9];
-        *offset += 8;
-        ((bytes[7] as u64) << 56) |
-        ((bytes[6] as u64) << 48) |
-        ((bytes[5] as u64) << 40) |
-        ((bytes[4] as u64) << 32) |
-        ((bytes[3] as u64) << 24) |
-        ((bytes[2] as u64) << 16) |
-        ((bytes[1] as u64) << 8) |
-        (bytes[0] as u64)
-    }
-
-    /// Parse a [ResponsePacket] into its' corresponding [ServerInfo].
+    /// Parse a [`ResponsePacket`] into its' corresponding [`ServerInfo`].
     pub fn parse(packet: ResponsePacket) -> Result<ServerInfo, SourceQueryError> {
         if packet.packet_type() != &PacketType::Response {
             return Err(SourceQueryError::AttemptParseEmptyPacket());
@@ -126,40 +84,40 @@ impl ServerInfo {
         let data: &Vec<u8> = &packet.body();
         let mut offset: usize = 0;
 
-        let protocol = Self::get_u8(data, &mut offset);
-        let hostname = Self::get_string(data, &mut offset)?;
-        let map = Self::get_string(data, &mut offset)?;
-        let folder = Self::get_string(data, &mut offset)?;
-        let game = Self::get_string(data, &mut offset)?;
-        let game_id = Self::get_u16(data, &mut offset);
-        let players = Self::get_u8(data, &mut offset);
-        let maxplayers = Self::get_u8(data, &mut offset);
-        let bots = Self::get_u8(data, &mut offset);
-        let server_type = char::from(Self::get_u8(data, &mut offset));
-        let server_env = char::from(Self::get_u8(data, &mut offset));
-        let password_protected = Self::get_u8(data, &mut offset) == 1;
-        let vac_enabled = Self::get_u8(data, &mut offset) == 1;
-        let version = Self::get_string(data, &mut offset)?;
+        let protocol = get_u8(data, &mut offset);
+        let hostname = get_string(data, &mut offset)?;
+        let map = get_string(data, &mut offset)?;
+        let folder = get_string(data, &mut offset)?;
+        let game = get_string(data, &mut offset)?;
+        let game_id = get_u16(data, &mut offset);
+        let players = get_u8(data, &mut offset);
+        let maxplayers = get_u8(data, &mut offset);
+        let bots = get_u8(data, &mut offset);
+        let server_type = char::from(get_u8(data, &mut offset));
+        let server_env = char::from(get_u8(data, &mut offset));
+        let password_protected = get_u8(data, &mut offset) == 1;
+        let vac_enabled = get_u8(data, &mut offset) == 1;
+        let version = get_string(data, &mut offset)?;
 
-        let edf = Self::get_u8(data, &mut offset);
+        let edf = get_u8(data, &mut offset);
         let edf_bitfield = edf.view_bits::<Msb0>();
 
         // 0x80 (Port)
         let port: Option<u16> = match edf_bitfield[0] {
-            true => Some(Self::get_u16(data, &mut offset)),
+            true => Some(get_u16(data, &mut offset)),
             false => None,
         };
         // 0x40 (Server Steam ID)
         let server_steam_id: Option<u64> = match edf_bitfield[1] {
-            true => Some(Self::get_u64(data, &mut offset)),
+            true => Some(get_u64(data, &mut offset)),
             false => None
         };
         // 0x20 (STV Port & Name)
         let stv_port: Option<u16>;
         let stv_name: Option<String>;
         if edf_bitfield[2] {
-            stv_port = Some(Self::get_u16(data, &mut offset));
-            stv_name = Some(Self::get_string(data, &mut offset)?);
+            stv_port = Some(get_u16(data, &mut offset));
+            stv_name = Some(get_string(data, &mut offset)?);
         } else {
             stv_port = None;
             stv_name = None;
@@ -167,7 +125,7 @@ impl ServerInfo {
         // 0x10 (Keywords)
         let keywords: Option<Vec<String>> = match edf_bitfield[3] {
             true => Some(
-                Self::get_string(data, &mut offset)?
+                get_string(data, &mut offset)?
                     .split(',')
                     .map(|k| k.to_owned())
                     .collect()
@@ -176,7 +134,7 @@ impl ServerInfo {
         };
         // 0x01 (GameID)
         let server_game_id: Option<u64> = match edf_bitfield[7] {
-            true => Some(Self::get_u64(data, &mut offset)),
+            true => Some(get_u64(data, &mut offset)),
             false => None
         };
 
@@ -206,10 +164,8 @@ impl ServerInfo {
     }
 }
 
-/// Query `host` with the Source Query Protocol A2S_INFO query.
-/// 
-/// If `timeout_dur` is `Some(Duration)`, each `timeout()` will use `timeout_dur`.
-/// The default is 5 seconds if `timeout_dur` is `None`.
+/// Query `host` with the Source Query Protocol A2S_INFO query,
+/// with the default timeout duration of 5 seconds.
 /// 
 /// Note that this timeout duration can occur 3 times (5 if challenged):
 /// - On socket connect
@@ -219,12 +175,36 @@ impl ServerInfo {
 /// 
 /// Example usage:
 /// ```
-/// let host: &str = "nyc-1.us.uncletopia.com:27015"; // Uncletopia New York City 4
-/// let info: ServerInfo = query(host, None).await?;
+/// let host: &str = "nyc-1.us.uncletopia.com:27015"; // Uncletopia New York City 1
+/// let info: ServerInfo = query(host).await?;
 /// ```
-pub async fn query(host: &str, timeout_dur: Option<Duration>) -> Result<ServerInfo, SourceQueryError> {
-    let timeout_dur: Duration = timeout_dur.unwrap_or(Duration::from_secs(5));
+/// 
+/// https://developer.valvesoftware.com/wiki/Server_queries#A2S_INFO
+pub async fn query(host: &str) -> Result<ServerInfo, SourceQueryError> {
+    _query(host, Duration::from_secs(DEFAULT_TIMEOUT_SECS)).await
+}
 
+/// Query `host` with the Source Query Protocol A2S_INFO query,
+/// with a timeout duration of `timeout_dur`.
+/// 
+/// Note that this timeout duration can occur 3 times (5 if challenged):
+/// - On socket connect
+/// - On packet send
+/// - On packet receive
+/// - Twice more on another send and receive, if challenged
+/// 
+/// Example usage:
+/// ```
+/// let host: &str = "nyc-1.us.uncletopia.com:27015"; // Uncletopia New York City 1
+/// let info: ServerInfo = query_timeout_duration(host, Duration::from_secs(2)).await?;
+/// ```
+/// 
+/// https://developer.valvesoftware.com/wiki/Server_queries#A2S_INFO
+pub async fn query_timeout_duration(host: &str, timeout_dur: Duration) -> Result<ServerInfo, SourceQueryError> {
+    _query(host, timeout_dur).await
+}
+
+async fn _query(host: &str, timeout_dur: Duration) -> Result<ServerInfo, SourceQueryError> {
     // just arbitrarily bind any port, doesn't matter really
     let sock: UdpSocket = UdpSocket::bind("0.0.0.0:0")
         .await
